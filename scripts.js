c942200c39043ff3e984d66c7bd9532880a03e38
Blain Crawford <blaincrawford@gmail.com>
2022-01-07 12:15:07 -0500

    just basic work with constructors

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let myDiv = document.getElementById('checking');
+let myH1 = document.createElement('h1');
+let myH1Also = document.createElement('h1');
+
+
+const Gams = function(quality, mind) {
+  this.quality = quality;
+  this.mind = mind;
+  this.compliment = function() {
+    this.quality = 'Afro'
+    return `The Gams has a nice ${this.quality}, and an incredible ${this.mind}!`;
+  }
+  this.blamsCompliment = function(){
+    return `The Blams has a nice ${this.quality}, and an incredible ${this.mind}!`
+  }
+};
+
+let gams = new Gams('Afro', 'mind');
+let blams = new Gams('smile', 'sense of humor')
+
+myH1.textContent = gams.compliment();
+myDiv.appendChild(myH1);
+
+myH1Also.textContent = blams.blamsCompliment();
+myDiv.appendChild(myH1Also);
+
